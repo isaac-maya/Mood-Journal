408a9e7fd8acd9d619326eaf20cc89dcef4a8342
Isaac Maya <106264448+isaac-maya@users.noreply.github.com>
2023-07-03 19:45:26 -0600

    Initial Commit

---
+++ Mood Journal/Mood_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_JournalApp.swift
+//  Mood Journal
+//
+//  Created by Happy Gilmore on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Mood_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
