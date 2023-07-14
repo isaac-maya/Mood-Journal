0f9a0187a68eb30f65ef28a6cbefe6b515d41466
Isaac Maya <106264448+isaac-maya@users.noreply.github.com>
2023-07-14 15:21:28 -0600

    Add confirmation of emoji saved

--- Mood Journal/MoodGaugeView.swift
+++ Mood Journal/MoodGaugeView.swift
@@ -1,7 +1,14 @@
 import SwiftUI
 
+enum Mood: String {
+    case happy = "😊"
+    case neutral = "😐"
+    case sad = "😔"
+}
+
 struct MoodGaugeView: View {
-    @State private var selectedFace: String = ""
+    @State private var selectedMood: Mood?
+    @State private var navigateToConfirmation: Bool = false
     
     private let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -24,41 +31,51 @@ struct MoodGaugeView: View {
             Spacer()
             
             VStack(spacing: 30) {
+                NavigationLink(destination: ConfirmationView(), isActive: $navigateToConfirmation) {
+                    EmptyView()
+                }
+                
                 Button(action: {
                     withAnimation {
-                        selectedFace = "😊" // Happy face
+                        selectedMood = .happy
+                        saveMood(.happy)
+                        navigateToConfirmation = true
                     }
                 }) {
-                    Text("😊")
+                    Text(Mood.happy.rawValue)
                         .font(.system(size: 120))
                         .frame(width: 140, height: 140)
-                        .background(selectedFace == "😊" ? Color.blue : Color.gray.opacity(0.5))
+                        .background(selectedMood == .happy ? Color.blue : Color.gray.opacity(0.5))
                         .foregroundColor(.white)
                         .clipShape(Circle())
                 }
                 
                 Button(action: {
                     withAnimation {
-                        selectedFace = "😐" // Neutral face
+                        selectedMood = .neutral
+                        saveMood(.neutral)
+                        navigateToConfirmation = true
                     }
                 }) {
-                    Text("😐")
+                    Text(Mood.neutral.rawValue)
                         .font(.system(size: 120))
                         .frame(width: 140, height: 140)
-                        .background(selectedFace == "😐" ? Color.blue : Color.gray.opacity(0.5))
+                        .background(selectedMood == .neutral ? Color.blue : Color.gray.opacity(0.5))
                         .foregroundColor(.white)
                         .clipShape(Circle())
                 }
                 
                 Button(action: {
                     withAnimation {
-                        selectedFace = "😔" // Sad face
+                        selectedMood = .sad
+                        saveMood(.sad)
+                        navigateToConfirmation = true
                     }
                 }) {
-                    Text("😔")
+                    Text(Mood.sad.rawValue)
                         .font(.system(size: 120))
                         .frame(width: 140, height: 140)
-                        .background(selectedFace == "😔" ? Color.blue : Color.gray.opacity(0.5))
+                        .background(selectedMood == .sad ? Color.blue : Color.gray.opacity(0.5))
                         .foregroundColor(.white)
                         .clipShape(Circle())
                 }
@@ -68,11 +85,27 @@ struct MoodGaugeView: View {
         }
         .navigationBarTitle("Mood Gauge")
     }
+    
+    func saveMood(_ mood: Mood) {
+        let currentDate = Date()
+        let key = dateFormatter.string(from: currentDate)
+        UserDefaults.standard.set(mood.rawValue, forKey: key)
+    }
+}
+
+struct ConfirmationView: View {
+    var body: some View {
+        Text("Mood has been saved!\nYou can close the app")
+            .font(.title)
+            .padding()
+    }
 }
 
 struct MoodGaugeView_Previews: PreviewProvider {
     static var previews: some View {
-        MoodGaugeView()
+        NavigationView {
+            MoodGaugeView()
+        }
     }
 }
 
