c08fb4b8c48ea20282b1c1fe3ad71cb56d6ab31e
Isaac Maya <106264448+isaac-maya@users.noreply.github.com>
2023-07-14 15:44:00 -0600

    Calendar shows emojis per day

--- Mood Journal/CalendarView.swift
+++ Mood Journal/CalendarView.swift
@@ -1,7 +1,41 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @State private var moods: [String: Mood] = [:]
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, yyyy"
+        return formatter
+    }()
+    
+    let daysInMonth = Calendar.current.range(of: .day, in: .month, for: Date())?.count ?? 0
+    let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text("This is the Calendar View")
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(1...daysInMonth, id: \.self) { day in
+                    let date = Calendar.current.date(from: DateComponents(year: Calendar.current.component(.year, from: Date()), month: Calendar.current.component(.month, from: Date()), day: day))!
+                    let key = dateFormatter.string(from: date)
+                    Text("\(day)\n\(moods[key]?.rawValue ?? "")")
+                        .frame(width: 40, height: 60)
+                        .border(Color.gray)
+                }
+            }
+        }
+        .onAppear {
+            loadMoods()
+        }
+    }
+    
+    func loadMoods() {
+        for day in 1...daysInMonth {
+            let date = Calendar.current.date(from: DateComponents(year: Calendar.current.component(.year, from: Date()), month: Calendar.current.component(.month, from: Date()), day: day))!
+            let key = dateFormatter.string(from: date)
+            if let moodString = UserDefaults.standard.string(forKey: key), let mood = Mood(rawValue: moodString) {
+                moods[key] = mood
+            }
+        }
     }
 }
+
